2b1ee8f94bf308460d9ec3261d92f544d6c95db8
moffa1953 <wmoffa@gmail.com>
2018-01-14 08:17:04 -0500

    initial git

---
+++ keys.js
@@ -0,0 +1,13 @@
+		console.log('this is loaded');
+
+		exports.twitter = {
+               consumer_key: process.env.0C5pgHd5HAXuKbI7UriH8LTED,
+               consumer_secret: process.env.5uQC2gDj1DzbDv4XNmLt8SN1QJ8ty81RufDYoDhyOApuKsbzS3,
+               access_token_key: process.env.49130412-buORjl1iP2wXAikyKnFSnxalpJR6hYsJLoEfsTtPQ,
+               access_token_secret: process.env.49130412-buORjl1iP2wXAikyKnFSnxalpJR6hYsJLoEfsTtPQ
+		};
+
+		exports.spotify = { https:
+  				id: process.env.SPOTIFY_ID,
+  				secret: process.env.SPOTIFY_SECRET
+		};
